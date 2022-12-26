93b74fda3a53ca62813c11abf0f636e55f28292b
Lohuama <limalorruama98@gmail.com>
2022-12-26 13:01:30 -0300

    EstruturaLogin-Desktop completo

--- componentes/login/index.js
+++ componentes/login/index.js
@@ -18,6 +18,7 @@ export default function Login(){
                     src={imagemLogo}
                     alt='logotipo'
                     layout="fill"
+                    className="logo"
                 />
             </div>
 
