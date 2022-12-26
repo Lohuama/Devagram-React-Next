8c1e28dd57085c55b4bc99e665590aff1911f9d9
Lohuama <limalorruama98@gmail.com>
2022-12-26 12:14:43 -0300

    EstruturaLogin-part2

--- componentes/login/index.js
+++ componentes/login/index.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import InputPublico from "../inputPublico";
+import Image from "next/image";
+import Botao from '../botao'
+import Link from "next/link";
 import imagemEnvelope from '../../public/images/envelope.svg'
-
+import imagemChave from '../../public/images/chave.svg'
+import imagemLogo from '../../public/images/logo.svg'
 
 export default function Login(){
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
     return (
         <section className={`paginaLogin paginaPublica`}>
             <div className="logoContainer">
-
+                <Image 
+                    src={imagemLogo}
+                    alt='logotipo'
+                    layout="fill"
+                />
             </div>
 
             <div className="conteudoPaginaPublica">
@@ -15,9 +27,28 @@ export default function Login(){
                         imagem={imagemEnvelope}
                         texto="E-mail"
                         tipo="email"
-                        aoAlterarValor={() => console.log('digitando email')}
+                        aoAlterarValor={e => setEmail(e.target.value)}
+                        valor={email}
+                    />
+                    <InputPublico 
+                        imagem={imagemChave}
+                        texto="Senha"
+                        tipo="password"
+                        aoAlterarValor={(e => setSenha(e.target.value))}
+                        valor={senha}
+                    />
+                    <Botao 
+                        texto="Login"
+                        tipo="submit"
+                        desabilitado={false}
                     />
                 </form>
+
+                <div className="rodapePaginaPublica">
+                    <p>Não posuui uma conta?</p>
+                    <Link href="/cadastro">Faça seu cadastro agora!</Link>
+                </div>
+
             </div>
         </section>
     );
