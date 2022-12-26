8924b38bf2b185a8fc21dbd45eb1b3f9c9d2e419
Lohuama <limalorruama98@gmail.com>
2022-12-26 11:39:22 -0300

    EstruturaLogin-parte1

---
+++ componentes/login/index.js
@@ -0,0 +1,24 @@
+import InputPublico from "../inputPublico";
+import imagemEnvelope from '../../public/images/envelope.svg'
+
+
+export default function Login(){
+    return (
+        <section className={`paginaLogin paginaPublica`}>
+            <div className="logoContainer">
+
+            </div>
+
+            <div className="conteudoPaginaPublica">
+                <form>
+                    <InputPublico 
+                        imagem={imagemEnvelope}
+                        texto="E-mail"
+                        tipo="email"
+                        aoAlterarValor={() => console.log('digitando email')}
+                    />
+                </form>
+            </div>
+        </section>
+    );
+}
